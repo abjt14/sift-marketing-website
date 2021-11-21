b570dc55821af474c8ee411b3f7b2575e5009234
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2021-11-20 21:08:07 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+			}
+		]
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, './')
+		},
+		compress: true,
+		port: 9000
+	}
+}
