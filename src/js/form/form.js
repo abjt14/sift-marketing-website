47ff0b0bfc6c8e289c708353bc87092b9e4201aa
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2021-11-24 13:18:06 -0800

    several additions and changes

---
+++ src/js/form/form.js
@@ -0,0 +1,22 @@
+const formInit = () => {
+	const scriptURL = 'https://script.google.com/macros/s/AKfycbxeudUhSn3AllxUXtdxgJswN06wg0aBC8v1gZcoLHScyzK01WWsv5nc6f1ESQJMc4_6/exec'
+  const form = document.forms['form']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => formSuccessAction())
+      .catch(error => console.error('Error!', error.message))
+  })
+
+	document.querySelector('#form-reset').addEventListener('click', (e) => {
+		formSuccessAction()
+	})
+}
+
+const formSuccessAction = () => {
+	const successCont = document.querySelector('#form-success-cont')
+	successCont.classList.toggle('display-none')
+}
+
+export { formInit }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import '../sass/style.scss'
 import { customScrollToImplementation } from './custom-scroll-to/customScrollToImplementation'
 import { navLinkScrollTrigger } from './nav-link-scroll-trigger/navLinkScrollTrigger'
+import smoothscroll from 'smoothscroll-polyfill'
+import { formInit } from './form/form'
 
+// kick off the polyfill!
+smoothscroll.polyfill();
 
 // onclick links
 customScrollToImplementation()
@@ -18,30 +22,15 @@ navLinkScrollTrigger()
 // mobile menu
 document.querySelector('#mobile-menu-toggle').addEventListener('click', (e) => {
 	document.querySelector('#header-mobile .menu').classList.toggle('active');
+	document.querySelector('body').classList.toggle('overflow-hidden');
 })
 document.querySelectorAll('html body #header-mobile .menu a').forEach(link => {
 	link.addEventListener('click', (e) => {
 		document.querySelector('#header-mobile .menu').classList.remove('active');
+		document.querySelector('body').classList.toggle('overflow-hidden');
 	})
 })
 // mobile menu end
-
-
-
-// nav menu onclick
-// const navMenuLinks = document.querySelectorAll('html body header #links-cont a')
-// navMenuLinks.forEach(link => {
-// 	link.addEventListener('click', (e) => {
-// 		navMenuLinks.forEach(linkInner => {
-// 			if (linkInner !== link) {
-// 				linkInner.classList.remove('active-2')
-// 			} else {
-// 				linkInner.classList.add('active-2')
-// 			}
-// 		})
-// 	})
-// })
-// nav menu onclick end
 
 
 
@@ -57,18 +46,21 @@ document.querySelector('html body header .logo-cont .img-cont').addEventListener
 
 
 // toggle header menu
-// let lastScrollTop;
-// const header = document.querySelector('header');
+let lastScrollTop;
+const header = document.querySelector('header');
 
-// window.addEventListener('scroll',function(){
-//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener('scroll',function(){
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-// 	if(scrollTop > lastScrollTop){
-// 		header.style.transform = 'translateY(-100%)';
-// 	} else{
-// 		header.style.transform = 'translateY(0%)';
-// 	}
+	if(scrollTop > lastScrollTop){
+		header.style.transform = 'translateY(-100%)';
+	} else{
+		header.style.transform = 'translateY(0%)';
+	}
 
-//   lastScrollTop = scrollTop;
-// });
+  lastScrollTop = scrollTop;
+});
 // toggle header menu end
+
+
+formInit()
