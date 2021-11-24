fe63c9fcb4a35a39e2604129037d4445a95c5511
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2021-11-23 16:51:44 -0800

    mobile font size fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../sass/style.scss'
+
+
+// onclick go to top
+document.querySelector('header .logo-cont .img-cont').addEventListener('click', (e) => {
+	window.scrollTo({top: 0, behavior: 'smooth'});
+})
